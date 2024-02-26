f0b5db941719e4ad31cfef1d3f46657a0ff017fa
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2024-02-26 20:47:24 +0700

    Mixins
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script>
+import flash from '@/mixins/flash'
+
+export default {
+    mixins: [flash]
+}
+</script>
+
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+
+        <button @click="flash('Flash from about page!')">Click me!</button>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+import TheWelcome from '../components/TheWelcome.vue'
+import flash from '@/mixins/flash'
+
+export default {
+    components: {
+        TheWelcome
+    },
+    mixins: [flash]
+}
+</script>
+
+<template>
+    <main>
+        <TheWelcome />
+
+        <button @click="flash('Flash from homepage!')">Click me!</button>
+    </main>
+</template>
